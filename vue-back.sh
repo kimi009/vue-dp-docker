5f55b89fe6c9cb412b150ef80c4c0ae1017a33a2
chenfuye <chenfuye@vanvy.cn>
2019-10-17 21:53:39 +0800

    test

--- vue-back.sh
+++ vue-back.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-WORK_PATH = "/web/vue-server"
+WORK_PATH='/web/vue-server'
 cd $WORK_PATH
 echo '先清除老代码'
-git reset --hard origin/master
-git clean -f
-echo '拉取最新的代码'
-git pull origin master
-echo '开始构建'
-docker build -t vue-server .
-echo '停止旧容器并删除旧容器'
-docker stop vue-server-container
-docker rm vue-server-container
-echo '启动新容器'
-docker container run -p 3009:3009 --name vue-server-container -d vue-back
